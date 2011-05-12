f24a59e57d45ca7966c35a2bdf0f4dadbdbdda46
Cyan Ogilvie <cyan@tirion.(none)>
2011-05-12 17:40:07 +0200

    Changed the method of preserving and replacing commands in the target interp that the DSL commands shadow to enable restoring commands that weren't aliases (like the native commands).  Added a list of reserved commands that cannot be used as DSL commands because they are required for the saving and restoring of the existing shadowed commands.

--- dsl.tcl
+++ dsl.tcl
@@ -1,5 +1,3 @@
-#!/usr/bin/env kbskit8.6
-
 # vim: ft=tcl foldmethod=marker foldmarker=<<<,>>> ts=4 shiftwidth=4
 
 namespace eval dsl {}
@@ -16,25 +14,59 @@ proc dsl::decomment {in} { #<<<
 }
 
 #>>>
+# Reserved dsl commands:
+#	if
+#	info
+#	rename
+#	apply
+#	foreach
+#	namespace
 proc dsl::dsl_eval {interp dsl_commands dsl_script args} { #<<<
 	set aliases_old	{}
-	foreach {cmdname cmdargs cmdbody} [dsl::decomment $dsl_commands] {
-		set alias	[interp alias $interp $cmdname]
-		if {$alias eq ""} {
-			dict set aliases_old $cmdname [list {}]
-		} else {
-			dict set aliases_old $cmdname [list [interp target $interp $cmdname] $alias]
-		}
-
-		interp alias $interp $cmdname {} apply [list $cmdargs $cmdbody [uplevel {namespace current}]] {*}$args
-	}
-
+	set tmpns		::_dsl_[incr ::dsl::seq]
+	set cmds		{}
 	try {
-		interp eval $interp $dsl_script
-	} finally {
-		dict for {cmdname oldalias} $aliases_old {
-			interp alias $interp $cmdname {*}$oldalias
+		interp eval $interp [format {namespace eval %s {}} [list $tmpns]]
+		foreach {cmdname cmdargs cmdbody} [dsl::decomment $dsl_commands] {
+			if {$cmdname in {if info rename apply foreach namespace}} {
+				error "\"$cmdname\" is a reserved word and cannot be used for a DSL command"
+			}
+			interp eval $interp [format {
+				if {[info commands ::%1$s] ne ""} {
+					rename ::%1$s %2$s::%1$s
+				}
+			} [list $cmdname] [list $tmpns]]
+			lappend cmds	$cmdname
+			#set alias	[interp alias $interp $cmdname]
+			#if {$alias eq ""} {
+			#	dict set aliases_old $cmdname [list {}]
+			#} else {
+			#	dict set aliases_old $cmdname [list [interp target $interp $cmdname] $alias]
+			#}
+
+			interp alias $interp $cmdname {} apply [list $cmdargs $cmdbody [uplevel {namespace current}]] {*}$args
 		}
+
+		if {$interp eq {}} {
+			apply [list {} $dsl_script [uplevel 1 {namespace current}]]
+		} else {
+			interp eval $interp $dsl_script
+		}
+	} finally {
+		#dict for {cmdname oldalias} $aliases_old {
+		#	interp alias $interp $cmdname {*}$oldalias
+		#}
+		interp eval $interp [format {
+			apply {{} {
+				foreach cmd %2$s {
+					rename ::$cmd {}
+					if {[info commands %1$s::$cmd] ne ""} {
+						rename %1$s::$cmd ::$cmd
+					}
+				}
+				namespace delete %1$s
+			}}
+		} [list $tmpns] [list $cmds]]
 	}
 }
 
